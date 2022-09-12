8e9189e8fc4edf21c64ae259b6744b872412a021
felipefalkiner <felipe.magosso@gmail.com>
2022-09-11 21:26:10 -0300

    install @types/express

---
+++ server/example-typescript.ts
@@ -0,0 +1,11 @@
+interface Ad{
+    id: string;
+    name: string;
+    createdAt: Date;
+
+}
+
+function calculaHaQuantoTempoOAnuncioFoiPublicado(ad: Ad) {
+    // calculo ha quantos dias foi postado  
+}
+
