e9b5e0db76b51e02d464f6290e8c0b6a417bfa52
felipefalkiner <felipe.magosso@gmail.com>
2022-09-11 21:35:33 -0300

    created web react project/folder with vite

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,8 @@ app.get('/ads', (request, response) => {
         { id: 1, name: 'Anúncio 1'},
         { id: 2, name: 'Anúncio 2'},
         { id: 3, name: 'Anúncio 3'},
-        { id: 4, name: 'Anúncio 4'}
+        { id: 4, name: 'Anúncio 4'},
+        { id: 5, name: 'Anúncio 5'}
     ])
 })
 
