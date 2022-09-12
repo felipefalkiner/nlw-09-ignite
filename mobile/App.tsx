8ad86eff919101cab92f39afb688b2d33f040adb
felipefalkiner <felipe.magosso@gmail.com>
2022-09-11 23:18:57 -0300

    mobile testing

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Salve meus amigos!</Text>
+
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Hello World" />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+interface ButtonProps{
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return(
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
