198904ee39c749d06e829c06ca44218aae659c59
felipefalkiner <felipe.magosso@gmail.com>
2022-09-11 22:40:25 -0300

    understanding React Components and properties

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,29 @@
 // React usa JSX: JavaScript + XML (HTML)
 // Consigo fazer o JS retornar HTML/CSS/JSssss
 
+// Componentes / Propriedades
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
 function App() {
-  return <h1>Hello NWL</h1>
+  return (
+    <div>
+      <Button title="Send 1"/>
+      <Button title="Send 2"/>
+      <Button title="Send 3"/>
+    </div>
+  
+  )
 }
 
 export default App
