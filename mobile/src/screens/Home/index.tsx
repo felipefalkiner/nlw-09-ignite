a9c7c0f9dc15ad86ff9e3cc2681a86f804cf97f5
felipefalkiner <felipe.magosso@gmail.com>
2022-09-12 23:17:03 -0300

    GameCard component created

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,10 @@
 import { View, Image } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
+import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
@@ -18,7 +20,9 @@ export function Home() {
         subtitle="Selecione o game que deseja jogar..."
       />
 
-
+      <GameCard 
+        data={GAMES[0]}
+      />
 
     </View>
   );
