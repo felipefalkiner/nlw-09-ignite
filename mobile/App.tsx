778575d63f24737f5e591461a5d4ccd5776daa63
felipefalkiner <felipe.magosso@gmail.com>
2022-09-12 22:32:51 -0300

    created background component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,40 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Salve meus amigos!</Text>
+    <Background>
 
-      <Button title="Send 1" />
-      <Button title="Send 2" />
-      <Button title="Send 3" />
-      <Button title="Hello World" />
-
-      <StatusBar style="auto" />
-    </View>
+    </Background>
   );
 }
-
-interface ButtonProps{
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
