2b4e482d6abbc15daf91c5086c0481c06bf91f19
felipefalkiner <felipe.magosso@gmail.com>
2022-09-12 22:08:30 -0300

    changed on App.tsx

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,16 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
   },
   plugins: [],
 }
