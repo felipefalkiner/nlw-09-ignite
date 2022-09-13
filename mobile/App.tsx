388f3be1a50c322b3459dce877c128e8e73f7274
felipefalkiner <felipe.magosso@gmail.com>
2022-09-12 22:48:48 -0300

    Created Loading component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,24 @@
 import { StatusBar } from 'react-native';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+ } from '@expo-google-fonts/inter';
 
+import { Home } from './src/screens/Home'; 
+import { Loading } from './src/components/Loading';
 import { Background } from './src/components/Background';
 
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
   return (
     <Background>
       <StatusBar
@@ -10,6 +26,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+
+      { fontsLoaded ? <Home /> : <Loading />}
 
     </Background>
   );
