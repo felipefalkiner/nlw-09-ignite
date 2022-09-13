4e692ec793f370a0a9b5d3afd6475f72fb970b0a
felipefalkiner <felipe.magosso@gmail.com>
2022-09-12 22:39:11 -0300

    installed expo modules and App.tsx updates

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,15 @@
+import { StatusBar } from 'react-native';
+
 import { Background } from './src/components/Background';
 
 export default function App() {
   return (
     <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
 
     </Background>
   );
