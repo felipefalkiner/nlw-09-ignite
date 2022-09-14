f65b43e756af6e346fced26f3ce4dff244fd1d82
felipefalkiner <felipe.magosso@gmail.com>
2022-09-13 21:41:54 -0300

    created routes, installed prisma and created 2 migrations

---
+++ server/README.md
@@ -0,0 +1,32 @@
+# Back-end
+
+## Dev Start: npm run dev
+
+## Entidades
+
+
+## Game
+
+id
+title
+bannerUrl
+
+## Ad
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+## Casos de uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,27 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+// HTTP methods / API RESTful / HTTP Codes
+// GET, POST, PUT, PATCH, DELETE
+
+/** Parms
+* Query: /ads?page=2&sort=title | filtros, paginação, etc | pensa em compartilhar a URL com alguém
+* Route: /ads/5
+* Body: {} | Enviar vários dados de uma vez
+*/
+
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
+
     return response.json([
         { id: 1, name: 'Anúncio 1'},
         { id: 2, name: 'Anúncio 2'},
@@ -10,6 +29,12 @@ app.get('/ads', (request, response) => {
         { id: 4, name: 'Anúncio 4'},
         { id: 5, name: 'Anúncio 5'}
     ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+
+    return response.json([])
 })
 
 app.listen(3333)
