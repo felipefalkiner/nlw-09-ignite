374ed2fc6564afc3c9646d1cbaf8376fb07798e1
felipefalkiner <felipe.magosso@gmail.com>
2022-09-15 11:15:25 -0300

    created CreateAdBanner.tsx component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import { MagnifyingGlassPlus } from "phosphor-react";
 
 import logoImg from './assets/logo-nlw.svg';
 import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 function App() {
   return (
@@ -18,18 +19,7 @@ function App() {
 
       </div>      
       
-      <div className='pt-1.5 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-          <div>
-            <strong className='text-2xl text-white font-black block'>Não encontrou o seu duo?</strong>
-            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
-          </div>
-          <button className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3'>
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
 
     </div>
   
