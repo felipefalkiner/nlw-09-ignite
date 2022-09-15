7756d4821dc68ae405b6aa36ebe5010c06451c53
felipefalkiner <felipe.magosso@gmail.com>
2022-09-15 18:03:21 -0300

    reinstalled mobile project via expo but this time using npm instead yarn, the old project can be found on 'mobile-old' who had a bug that I couldn't solve | also Home is now fetching the API and changes on GameCard component to receive data from the API

---
+++ mobile-old/src/screens/Game/index.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { styles } from './styles';
+
+export function Game() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
