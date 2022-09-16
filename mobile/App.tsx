7673aa6f281de11d1c06a02e4085207dfc2d3459
felipefalkiner <felipe.magosso@gmail.com>
2022-09-16 10:16:02 -0300

    finished implementing Notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,6 +8,7 @@ import {
   Inter_900Black
  } from '@expo-google-fonts/inter';
  import { Subscription } from 'expo-modules-core'
+ import * as Notifications from 'expo-notifications';
 
 import { Routes } from './src/routes'; 
 import { Loading } from './src/components/Loading';
@@ -29,6 +30,25 @@ export default function App() {
 
   useEffect(() => {
     getPushNotificationToken();
+  });
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications
+    .addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+
+    responseNotificationListener.current = Notifications
+    .addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
   })
 
   return (
