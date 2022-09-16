a39ace305b61d8ce3f10abf36ef8fe55b82e71ba
felipefalkiner <felipe.magosso@gmail.com>
2022-09-16 02:16:22 -0300

    DuoMatch component copies Discord to clipboard

--- mobile/src/components/DuoMatch/index.tsx
+++ mobile/src/components/DuoMatch/index.tsx
@@ -1,10 +1,12 @@
-import { View, Modal, ModalProps, Text, TouchableOpacity } from 'react-native';
+import { View, Modal, ModalProps, Text, TouchableOpacity, Alert, ActivityIndicator } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons'
-import { CheckCircle } from 'phosphor-react-native'
+import { Activity, CheckCircle } from 'phosphor-react-native'
+import * as Clipboard from 'expo-clipboard';
 
 import { styles } from './styles';
 import { THEME } from '../../theme';
 import { Heading } from '../Heading';
+import { useState } from 'react';
 
 interface Props extends ModalProps {
   discord:string;
@@ -12,6 +14,15 @@ interface Props extends ModalProps {
 }
 
 export function DuoMatch({discord, onClose, ...rest}: Props) {
+  const [isCopying, setIsCopying] = useState(false);
+
+  async function handleCopyDiscordToClipboard(){
+    setIsCopying(true);
+    await Clipboard.setStringAsync(discord);
+    Alert.alert('Discord copiado!', 'Usuário copiado para a área de transferência.');
+    setIsCopying(false);
+  }
+
   return (
     <Modal
       animationType='fade'
@@ -50,9 +61,11 @@ export function DuoMatch({discord, onClose, ...rest}: Props) {
 
           <TouchableOpacity
             style={styles.discordButton}
+            onPress={handleCopyDiscordToClipboard}
+            disabled={isCopying}
           >
             <Text style={styles.discord}>
-              {discord}
+              {isCopying ? <ActivityIndicator color={THEME.COLORS.PRIMARY} /> : discord}
             </Text>
           </TouchableOpacity>
         </View>
